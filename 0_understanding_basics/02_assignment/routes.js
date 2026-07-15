5c747313680c269b746ca794b8c0105e21dae22a
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-07-14 17:28:49 -0700

    Add GET method for main page

--- 0_understanding_basics/02_assignment/routes.js
+++ 0_understanding_basics/02_assignment/routes.js
@@ -1,4 +1,21 @@
 const handelReq = (req,res)=>{
-
+    const url = req.url;
+    const method = req.method;
+    const users = [];
+    if (url === "/" && method === "GET"){
+        res.setHeader("Content-Type","text/html");
+        res.write(`
+        <html>
+            <head><title>Hello Dude</title></head>
+            <body>
+                <h1>hello from my node.js server!</h1>
+                <h2>make some user with the input below :D</h2>
+                <form action='/create-user' method='POST'><input type='text' name='createUser'><button type='submit'>Send</button></form>
+            </body>
+        </html>  
+            `);
+        res.end();
+    }
 }
+
 export{handelReq};
