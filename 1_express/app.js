4905938d7522239ca082089a0b124ca8e8b62800
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-07-16 23:02:24 -0700

    Add a simple middleware

--- 1_express/app.js
+++ 1_express/app.js
@@ -1,2 +1,20 @@
-import * as express from "express";
+import http from "http";
+import express from "express";
 const app = express();
+
+// adding a middleware function
+// next is used for traveling to another middlewere
+app.use((req,res,next)=>{
+    console.log("in the middleware!");
+    next(); // allowsthe req to continue in the next middleware
+});
+app.use((req,res,next)=>{
+    console.log("in another middleware!");
+})
+
+
+// u can pass express to http.createServer 
+// but it handles non
+// it only sets up a way of handling 
+const server = http.createServer(app);
+server.listen(3000);
