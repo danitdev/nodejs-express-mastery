625de53e690ae1e7c56d86c89f53aa8c69734b79
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-07-17 01:46:20 -0700

    Add page 404 to views and do the same process with the utils and path and sendFile...

--- 2_express_parsing_incoming_requests/app.js
+++ 2_express_parsing_incoming_requests/app.js
@@ -1,8 +1,9 @@
 import express from "express";
+import path from "path";
 import bodyParser from "body-parser";
 import {router as adminRouter} from "./Routes/admin.js";
 import {router as shopRouter} from "./Routes/shop.js";
-
+import rootDir from "./utils/path.js"
 
 const app = express();
 // this pass a middleware function and it does the whole body parsing we were used to do and then next() to them
@@ -13,7 +14,7 @@ app.use("/shop",shopRouter);
 // handling other pages
 app.use((req,res,next)=>{
     // u can chain status with send to set status
-    res.status(404).send("<h1>Page Not Found</h1>");
+    res.status(404).sendFile(path.join(rootDir,"Views","404.html"));
 })
 
 
