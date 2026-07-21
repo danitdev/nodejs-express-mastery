ddd9ff6bda11e16df5e6767696659037d67d1a48
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-07-20 19:37:39 -0700

    Add static files for external css

--- 2_express_parsing_incoming_requests/app.js
+++ 2_express_parsing_incoming_requests/app.js
@@ -8,6 +8,12 @@ import rootDir from "./utils/path.js"
 const app = express();
 // this pass a middleware function and it does the whole body parsing we were used to do and then next() to them
 app.use(bodyParser.urlencoded({extended: false}));
+// giving acess to users have this static files
+// can be images css files... 
+// but u have to remember now u are in public dir
+//and in html files if there is link u have to think u are in public and give the direction from there
+app.use(express.static(path.join(rootDir,"public")));
+
 
 app.use("/admin",adminRouter); //the "/admin" filter the path 
 app.use("/shop",shopRouter);
