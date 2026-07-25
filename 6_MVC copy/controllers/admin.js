3e64afd4d4b8d1f32a0ed86e3c04910f46c56d16
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-07-25 14:12:54 -0700

    Add making new product domain model base on new user req inputs

--- 6_MVC copy/controllers/admin.js
+++ 6_MVC copy/controllers/admin.js
@@ -4,7 +4,11 @@ export const getAddProduct = (req,res,next)=>{
     res.render("admin/add-product",{pageTitle:"Add Product",path:"/admin/add-product"});
 };
 export const postAddProduct = (req,res,next)=>{
-    const product = new Product(req.body.title);
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const desc = req.body.desc;
+    const price = req.body.price;
+    const product = new Product(title,imageUrl,desc,price);
     product.save();
     res.redirect("/shop");
 };
