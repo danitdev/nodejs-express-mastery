79023cf6e8d3b352e24309039d82d8106620e10e
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-07-25 14:02:52 -0700

    Add set the controller in router for admin.js

---
+++ 6_MVC copy/Routes/admin.js
@@ -0,0 +1,16 @@
+import express from "express";
+// add get and post controller
+import { getAddProduct,postAddProduct, getAdminProducts} from "../controllers/admin.js";
+// set router(router is like mini app)
+const router = express.Router();
+
+
+// using the controller getAddProduct
+router.get("/add-product",getAddProduct);
+//app.get is same as app.use but limit the req we have also app.post
+router.post("/add-product",postAddProduct);
+// admin/products => GET
+router.get("/products",getAdminProducts);
+
+
+export{router};
