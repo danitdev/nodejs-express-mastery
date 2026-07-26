88c8e89bb31388192842ca2ca54c7e4742f044bd
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-07-26 15:21:21 -0700

    feat: add getEditProduct

--- 7_dynamic_data_model/controllers/admin.js
+++ 7_dynamic_data_model/controllers/admin.js
@@ -1,8 +1,9 @@
 import { Product } from "../models/product.js";
 
 export const getAddProduct = (req,res,next)=>{
-    res.render("admin/add-product",{pageTitle:"Add Product",path:"/admin/add-product"});
+    res.render("admin/edit-product",{pageTitle:"Add Product",path:"/admin/add-product"});
 };
+
 export const postAddProduct = (req,res,next)=>{
     const title = req.body.title;
     const imageUrl = req.body.imageUrl;
@@ -11,6 +12,13 @@ export const postAddProduct = (req,res,next)=>{
     const product = new Product(title,imageUrl,desc,price);
     product.save();
     res.redirect("/");
+};
+export const getEditProduct = (req,res,next)=>{
+    const editMode = req.query.edit;
+    if(!editMode){
+        return res.redirect("/");
+    }
+    res.render("admin/edit-product",{pageTitle:"Edit Product",path:"/admin/edit-product",editing:editMode});
 };
 export const getAdminProducts = (req,res,next)=>{
     const products = Product.fetchAll((products)=>{
