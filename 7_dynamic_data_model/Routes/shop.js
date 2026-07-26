e8552869120a3cf24d23f28bd6793b20db01a65b
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-07-25 17:39:17 -0700

    feat: add get product by id to shop.js router

--- 7_dynamic_data_model/Routes/shop.js
+++ 7_dynamic_data_model/Routes/shop.js
@@ -1,10 +1,11 @@
 import express from "express";
 // add getShop controller
-import { getShop , getIndex , getCart, getCheckout,getOrders} from "../controllers/shop.js";
+import { getProducts,getProduct , getIndex , getCart, getCheckout,getOrders} from "../controllers/shop.js";
 const router = express.Router();
 
 router.get("/",getIndex);
-router.get("/products",getShop);
+router.get("/products",getProducts);
+router.get("/products/:productId",getProduct);
 router.get("/cart",getCart);
 router.get("/checkout",getCheckout)
 router.get("/orders",getOrders);
