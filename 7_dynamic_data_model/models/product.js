711cfecfb3387c6ffb165df05b8113b1d6174eaa
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-07-25 18:04:44 -0700

    feat: add findById function

--- 7_dynamic_data_model/models/product.js
+++ 7_dynamic_data_model/models/product.js
@@ -36,5 +36,11 @@ class Product{
     static fetchAll(cb){
         getProductsFromFile(cb);
     }
+    static findById(id,cb){
+        getProductsFromFile(products=>{
+            const product = products.find(p => p.id === id);
+            cb(product);
+        });
+    }
 }
 export{Product};
