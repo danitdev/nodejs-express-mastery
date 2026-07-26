a21ba086a74a1ce9897f6706713680cf3446d66f
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-07-26 13:16:46 -0700

    refactor: using new cart model in post cart method

--- 7_dynamic_data_model/controllers/shop.js
+++ 7_dynamic_data_model/controllers/shop.js
@@ -1,5 +1,5 @@
 import { Product } from "../models/product.js";
-
+import { Cart } from "../models/cart.js";
 
 export const getProducts = (req,res,next)=>{
     const products = Product.fetchAll((products)=>{
@@ -22,7 +22,9 @@ export const getCart = (req,res,next)=>{
 }
 export const postCart = (req,res,next)=>{
     const prodId = req.body.productId;
-    console.log(prodId);
+    Product.findById(prodId,(product)=>{
+        Cart.addProduct(prodId,product.price);
+    })
     res.redirect("/cart");
 };
 export const getCheckout = (req,res,next)=>{
