fc2df45746a58779c1ce2ae4ac52fed1c5c6c862
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-07-26 15:21:04 -0700

    feat: add edit-product route to admin.js router

--- 7_dynamic_data_model/Routes/admin.js
+++ 7_dynamic_data_model/Routes/admin.js
@@ -1,6 +1,6 @@
 import express from "express";
 // add get and post controller
-import { getAddProduct,postAddProduct, getAdminProducts} from "../controllers/admin.js";
+import { getAddProduct,postAddProduct, getAdminProducts, getEditProduct} from "../controllers/admin.js";
 // set router(router is like mini app)
 const router = express.Router();
 
@@ -9,6 +9,7 @@ const router = express.Router();
 router.get("/add-product",getAddProduct);
 //app.get is same as app.use but limit the req we have also app.post
 router.post("/add-product",postAddProduct);
+router.get("/edit-product/:productId",getEditProduct);
 // admin/products => GET
 router.get("/products",getAdminProducts);
 
