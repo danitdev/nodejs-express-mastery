c6b6233204c4f6fceea90ca2dd4911ddacca9be9
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-07-26 12:52:32 -0700

    feat: add post cart route to shop router

--- 7_dynamic_data_model/Routes/shop.js
+++ 7_dynamic_data_model/Routes/shop.js
@@ -1,12 +1,13 @@
 import express from "express";
 // add getShop controller
-import { getProducts,getProduct , getIndex , getCart, getCheckout,getOrders} from "../controllers/shop.js";
+import { getProducts,getProduct , getIndex , getCart, postCart, getCheckout,getOrders} from "../controllers/shop.js";
 const router = express.Router();
 
 router.get("/",getIndex);
 router.get("/products",getProducts);
 router.get("/products/:productId",getProduct);
 router.get("/cart",getCart);
+router.post("/cart",postCart);
 router.get("/checkout",getCheckout)
 router.get("/orders",getOrders);
 export{router};
