d38e4cc5b45ac0ecb8b48b2003632d9bad359eee
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-07-26 17:06:19 -0700

    refactor: passing null for id so that in save function creates a new id

--- 7_dynamic_data_model/controllers/admin.js
+++ 7_dynamic_data_model/controllers/admin.js
@@ -9,7 +9,7 @@ export const postAddProduct = (req,res,next)=>{
     const imageUrl = req.body.imageUrl;
     const desc = req.body.desc;
     const price = req.body.price;
-    const product = new Product(title,imageUrl,desc,price);
+    const product = new Product(null,title,imageUrl,desc,price);
     product.save();
     res.redirect("/");
 };
@@ -31,6 +31,16 @@ export const getEditProduct = (req,res,next)=>{
                 product:product});
         
     });
+};
+export const postEditProduct = (req,res,next)=>{
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDesc = req.body.desc;
+    const updatedProd = new Product(prodId,updatedTitle,updatedImageUrl,updatedDesc,updatedPrice);
+    updatedProd.save();
+    res.redirect("/admin/products");
 };
 export const getAdminProducts = (req,res,next)=>{
     const products = Product.fetchAll((products)=>{
