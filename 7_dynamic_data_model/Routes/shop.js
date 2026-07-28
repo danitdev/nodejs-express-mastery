115916622544e7b71b075575e01d8817f17cba85
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-07-28 12:19:55 -0700

    feat: add post cart delete product route to shop.js router

--- 7_dynamic_data_model/Routes/shop.js
+++ 7_dynamic_data_model/Routes/shop.js
@@ -1,13 +1,14 @@
 import express from "express";
 // add getShop controller
-import { getProducts,getProduct , getIndex , getCart, postCart, getCheckout,getOrders} from "../controllers/shop.js";
+import { getProducts,getProduct , getIndex , getCart, postCart, getCheckout,getOrders, postCartDeleteProduct} from "../controllers/shop.js";
 const router = express.Router();
 
 router.get("/",getIndex);
 router.get("/products",getProducts);
 router.get("/products/:productId",getProduct);
 router.get("/cart",getCart);
-router.post("/cart",postCart);
+router.post("/cart",postCart); 
+router.post("/cart-delete-item",postCartDeleteProduct);
 router.get("/checkout",getCheckout)
 router.get("/orders",getOrders);
 export{router};
