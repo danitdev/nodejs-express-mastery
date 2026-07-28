f540c9a5855f619ed9ccc7375f095dd21881cc9d
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-07-28 12:12:46 -0700

    feat: using getCart method to make the getCart route

--- 7_dynamic_data_model/controllers/shop.js
+++ 7_dynamic_data_model/controllers/shop.js
@@ -18,7 +18,19 @@ export const getIndex = (req,res,next)=>{
     });
 }
 export const getCart = (req,res,next)=>{
-    res.render("shop/cart",{path:"/cart",pageTitle:"Your Cart"})
+    Cart.getCart(cart=>{
+        Product.fetchAll(products=>{
+            const cartProducts = [];
+            for(const product of products){
+                const cartProductData = cart.products.find(prod => prod.id === product.id);
+                if(cartProductData){
+                    cartProducts.push({productData: product,qty: cartProductData.qty});
+                }
+            }
+            res.render("shop/cart",{path:"/cart",pageTitle:"Your Cart",products:cartProducts})
+        });
+    });
+
 }
 export const postCart = (req,res,next)=>{
     const prodId = req.body.productId;
