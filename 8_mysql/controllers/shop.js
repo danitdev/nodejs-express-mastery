44d2e7117e7680527ab7c447346aa66ad594ca74
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-07-28 16:57:37 -0700

    feat: rewriting logic for getProduct in shop.js controller

--- 8_mysql/controllers/shop.js
+++ 8_mysql/controllers/shop.js
@@ -10,9 +10,13 @@ export const getProducts = (req,res,next)=>{
 };
 export const getProduct = (req,res,next)=>{
     const prodId = req.params.productId;
-    Product.findById(prodId,product=>{
-        res.render("shop/product-detail",{product:product,pageTitle:product.title,path:"/products"})
-    });
+    Product.findById(prodId)
+        .then(([rows,fieldData])=>{
+            const product = rows[0];
+            res.render("shop/product-detail",{product:product,pageTitle:product.title,path:"/products"})
+            console.log("poplo???");
+        })
+        .catch(err=>{console.log(err)})
 };
 export const getIndex = (req,res,next)=>{
     const products = Product.fetchAll()
