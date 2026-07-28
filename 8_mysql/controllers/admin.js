38ff4d87e3fa093b49a6ecdb9a84dfd6fdd6c248
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-07-28 16:57:15 -0700

    refactor: changing desc prop to description cause desc collide with mysql collide key word

--- 8_mysql/controllers/admin.js
+++ 8_mysql/controllers/admin.js
@@ -7,9 +7,9 @@ export const getAddProduct = (req,res,next)=>{
 export const postAddProduct = (req,res,next)=>{
     const title = req.body.title;
     const imageUrl = req.body.imageUrl;
-    const desc = req.body.desc;
+    const description = req.body.description;
     const price = req.body.price;
-    const product = new Product(null,title,imageUrl,desc,price);
+    const product = new Product(null,title,imageUrl,description,price);
     product.save()
         .then(()=>{
             res.redirect("/");
@@ -40,8 +40,8 @@ export const postEditProduct = (req,res,next)=>{
     const updatedTitle = req.body.title;
     const updatedPrice = req.body.price;
     const updatedImageUrl = req.body.imageUrl;
-    const updatedDesc = req.body.desc;
-    const updatedProd = new Product(prodId,updatedTitle,updatedImageUrl,updatedDesc,updatedPrice);
+    const updatedDescription = req.body.description;
+    const updatedProd = new Product(prodId,updatedTitle,updatedImageUrl,updatedDescription,updatedPrice);
     updatedProd.save();
     res.redirect("/admin/products");
 };
