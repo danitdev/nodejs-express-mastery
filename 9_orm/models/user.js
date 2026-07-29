c50017fb3b0c17bd4f2bd729b3ade467fedc9e13
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-07-29 14:02:38 -0700

    feat: add user model

---
+++ 9_orm/models/user.js
@@ -0,0 +1,18 @@
+import { Sequelize,DataTypes  } from "sequelize";
+import { sequelize } from "../utils/database";
+export const User = sequelize.define("user",{
+    id:{
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name:{
+        type:DataTypes.STRING,
+        allowNull: false
+    },
+    email:{
+        type:DataTypes.STRING,
+        allowNull: false
+    }
+});
