7bad278624b3d91321a841f0c9f4050eb879fe5e
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-07-28 19:33:24 -0700

    feat: findByPk for fetching one product rendering its details

--- 9_orm/controllers/shop.js
+++ 9_orm/controllers/shop.js
@@ -4,16 +4,19 @@ import { Cart } from "../models/cart.js";
 export const getProducts = (req,res,next)=>{
     // find all is fetching the all from table
     Product.findAll().then(products=>{
-        res.render("shop/index",{prods: products,pageTitle:"All Products",path:"/products"});
+        res.render("shop/product-list",{prods: products,pageTitle:"All Products",path:"/products"});
     }).catch(err=>console.log(err));
 };
 export const getProduct = (req,res,next)=>{
     const prodId = req.params.productId;
-    Product.findById(prodId)
-        .then(([rows,fieldData])=>{
-            const product = rows[0];
-            res.render("shop/product-detail",{product:product,pageTitle:product.title,path:"/products"})
-            console.log("poplo???");
+    //alternative way using findAll and filter it 
+    // Product.findAll({where:{id:prodId}}).then(products=>{
+        // res.render("shop/product-detail",{product:products[0],pageTitle:products[0].title,path:"/products"});
+    // }).catch(err=>console.log(err));
+    // find by pk is for fetching one from table using id
+    Product.findByPk(prodId)
+        .then((product)=>{
+            res.render("shop/product-detail",{product:product,pageTitle:product.title,path:"/products"});
         })
         .catch(err=>{console.log(err)})
 };
