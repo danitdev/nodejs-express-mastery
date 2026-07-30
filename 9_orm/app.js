a65fbea60224329ca24f8b75a4f1e73975bd8662
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-07-29 19:13:21 -0700

    feat: define relations with the order model

--- 9_orm/app.js
+++ 9_orm/app.js
@@ -11,7 +11,8 @@ import { Product } from "./models/product.js";
 import { User } from "./models/user.js";
 import { Cart } from "./models/cart.js";
 import { CartItem } from "./models/cart-item.js";
-
+import { Order } from "./models/order.js";
+import { OrderItem } from "./models/order-item.js";
 
 // add 404 controller
 const app = express();
@@ -50,9 +51,14 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product,{through: CartItem});
 Product.belongsToMany(Cart,{through: CartItem});
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {through: OrderItem});
+
+
 
 //sync the models to db and creating the table in db
-sequelize.sync({force: true})
+sequelize.sync()
     .then(result=>{
         return User.findByPk(1);
         // console.log(result);
