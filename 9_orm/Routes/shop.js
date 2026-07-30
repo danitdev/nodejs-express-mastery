cc96c596b1675300feb327b4063661b2706eeb51
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-07-29 20:10:44 -0700

    feat: add postorder route to shop.js router

--- 9_orm/Routes/shop.js
+++ 9_orm/Routes/shop.js
@@ -1,6 +1,6 @@
 import express from "express";
 // add getShop controller
-import { getProducts,getProduct , getIndex , getCart, postCart, getCheckout,getOrders, postCartDeleteProduct} from "../controllers/shop.js";
+import { getProducts,getProduct , getIndex , getCart, postCart, getCheckout,getOrders, postCartDeleteProduct, postOrder} from "../controllers/shop.js";
 const router = express.Router();
 
 router.get("/",getIndex);
@@ -10,5 +10,7 @@ router.get("/cart",getCart);
 router.post("/cart",postCart); 
 router.post("/cart-delete-item",postCartDeleteProduct);
 router.get("/checkout",getCheckout)
+router.post("/create-order",postOrder);
 router.get("/orders",getOrders);
+
 export{router};
