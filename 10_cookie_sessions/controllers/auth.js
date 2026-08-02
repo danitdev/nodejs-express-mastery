cf1f028cfa38b5b81c5f130c576221948997ad4e
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-02 16:22:12 -0700

    feat: add logout to auth.js controller

--- 10_cookie_sessions/controllers/auth.js
+++ 10_cookie_sessions/controllers/auth.js
@@ -21,3 +21,9 @@ export const postLogin = (req,res,next)=>{
         })
         .catch(err=>console.log(err));
 }
+export const postLogout = (req,res,next)=>{
+    req.session.destroy((err)=>{
+        console.log(err);
+        res.redirect("/");
+    });
+};
