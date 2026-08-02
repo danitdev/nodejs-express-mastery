134af63376b492a51c5df5d767a2c357bfa3e336
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-02 16:21:56 -0700

    feat: add route for logout to auth.js router

--- 10_cookie_sessions/Routes/auth.js
+++ 10_cookie_sessions/Routes/auth.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getLogin, postLogin } from "../controllers/auth.js";
+import { getLogin, postLogin, postLogout } from "../controllers/auth.js";
 
 const router = express.Router();
 
 router.get("/login",getLogin);
 router.post("/login",postLogin);
-
+router.post("/logout",postLogout);
 export{router};
