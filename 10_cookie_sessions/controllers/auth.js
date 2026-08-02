4827adadd5f53fe9a4568e687e9ca0a44eeca168
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-02 11:32:27 -0700

    feat: extract isLoggedIn from cookie and set the header in postLogin

--- 10_cookie_sessions/controllers/auth.js
+++ 10_cookie_sessions/controllers/auth.js
@@ -1,3 +1,10 @@
 export const getLogin = (req,res,next)=>{
-    res.render("auth/login",{path:"/login",pageTitle:"login"});
+    const isLoggedIn = req.get("Cookie").split("=")[1];
+    console.log(isLoggedIn);
+    res.render("auth/login",{path:"/login",pageTitle:"login",isAuth:isLoggedIn});
 };
+export const postLogin = (req,res,next)=>{
+    // setting a cookie
+    res.setHeader("Set-Cookie","loggedIn=true");
+    res.redirect("/");
+}
