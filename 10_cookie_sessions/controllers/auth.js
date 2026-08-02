5a7aa3f89a14e8ee99b829c1bc25f3ad34c1e028
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-02 15:35:50 -0700

    feat: set userId and isLoggedIn to the session

--- 10_cookie_sessions/controllers/auth.js
+++ 10_cookie_sessions/controllers/auth.js
@@ -1,10 +1,23 @@
+
+import { User } from "../models/user.js";
+
 export const getLogin = (req,res,next)=>{
-    const isLoggedIn = req.get("Cookie").split("=")[1];
-    console.log(isLoggedIn);
-    res.render("auth/login",{path:"/login",pageTitle:"login",isAuth:isLoggedIn});
+    // const isLoggedIn = req.get("Cookie").split("=")[1];
+    // console.log(isLoggedIn);
+    //then u can pass isAuth as value isLoggedIn but this
+    //is a bad example cuz the thing is u can manipulate data in cookies 
+    console.log(req.session.isLoggedIn);
+    res.render("auth/login",{path:"/login",pageTitle:"login",isAuth:req.session.isLoggedIn});
 };
 export const postLogin = (req,res,next)=>{
     // setting a cookie
-    res.setHeader("Set-Cookie","loggedIn=true");
-    res.redirect("/");
+    // res.setHeader("Set-Cookie","loggedIn=true; Max-Age=10");
+    User.findByPk(1)
+        .then(user=>{
+            // making a param for user
+            req.session.isLoggedIn = true;
+            req.session.userId = user.id;
+            res.redirect("/");
+        })
+        .catch(err=>console.log(err));
 }
