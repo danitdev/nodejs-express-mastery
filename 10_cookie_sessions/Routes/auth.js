d054a7b1973a6123ea5d1d23d7617a23a3f6f89e
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-02 18:06:19 -0700

    feat: add get signup to auth.js router

--- 10_cookie_sessions/Routes/auth.js
+++ 10_cookie_sessions/Routes/auth.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getLogin, postLogin, postLogout } from "../controllers/auth.js";
+import { getLogin, getSignup, postLogin, postLogout } from "../controllers/auth.js";
 
 const router = express.Router();
 
 router.get("/login",getLogin);
 router.post("/login",postLogin);
 router.post("/logout",postLogout);
+router.get("/signup",getSignup);
+
 export{router};
