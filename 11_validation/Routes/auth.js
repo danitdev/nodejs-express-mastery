d982e3b78f774ece9688986dd0e1c114bd01ced8
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-05 14:26:53 -0700

    feat: add validation for password

--- 11_validation/Routes/auth.js
+++ 11_validation/Routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {check} from "express-validator";
+import {check, body} from "express-validator";
 import { getLogin, getNewPassword, getReset, getSignup, postLogin, postLogout, postNewPassword, postReset, postSignup } from "../controllers/auth.js";
 
 const router = express.Router();
@@ -9,17 +9,20 @@ router.post("/login",postLogin);
 router.post("/logout",postLogout);
 router.get("/signup",getSignup);
 //add validation as middleware check for email with isEmail function
-router.post("/signup",check("email")
-                    .isEmail()
-                    .withMessage("Please enter a valid email.")
-                    //a custom validator
-                    .custom((value,{req})=>{
-                        if(value === "test@test.com"){
-                            throw new Error("this email address is forbidden.");
-                        }
-                        return true;
-                    }),
-                    postSignup);
+router.post("/signup",
+    [check("email")
+    .isEmail()
+    .withMessage("Please enter a valid email.")
+    //a custom validator
+    .custom((value,{req})=>{
+        if(value === "test@test.com"){
+            throw new Error("this email address is forbidden.");
+        }
+        return true;
+    }),
+    body("password","please enter a password using numbers and text and at least 5 characters.").isLength({min5}).isAlphanumeric()
+    ],
+    postSignup);
 router.get("/reset",getReset);
 router.post("/reset",postReset);
 router.get("/reset/:token",getNewPassword);
