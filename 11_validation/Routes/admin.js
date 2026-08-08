3df19c9a4f1f9633276db4713b24fd0e1fe0293c
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-08 00:20:18 -0700

    feat: add validation for add and edit product

--- 11_validation/Routes/admin.js
+++ 11_validation/Routes/admin.js
@@ -3,7 +3,7 @@ import express from "express";
 import { getAddProduct,postAddProduct, getAdminProducts, getEditProduct, postEditProduct, postDeleteProduct} from "../controllers/admin.js";
 // route protection using middleware
 import { isAuth } from "../middleware/is-auth.js";
-
+import {check, body} from "express-validator"
 // set router(router is like mini app)
 const router = express.Router();
 
@@ -13,8 +13,30 @@ router.get("/add-product",isAuth,getAddProduct);
 //app.get is same as app.use but limit the req we have also app.post
 // admin/products => GET
 router.get("/products",isAuth,getAdminProducts);
-router.post("/add-product",isAuth,postAddProduct);
-router.get("/edit-product/:productId",isAuth,getEditProduct);
+router.post("/add-product",
+    [
+        body("title")
+            .isAlphanumeric().isLength({min:3}).trim(),
+        body("imageUel")
+            .isURL(),
+        body("price")
+            .isFloat(),
+        body("description")
+            .isLength({min:5,max:500})
+    ],isAuth,postAddProduct);
+router.get("/edit-product/:productId"
+    , 
+    [
+        body("title")
+            .isString().isLength({min:3}).trim(),
+        body("imageUel")
+            .isURL(),
+        body("price")
+            .isFloat(),
+        body("description")
+            .isLength({min:5,max:500})
+    ]
+    ,isAuth,getEditProduct);
 router.post("/edit-product",isAuth,postEditProduct);
 router.post("/delete-product",isAuth,postDeleteProduct);
 export{router};
