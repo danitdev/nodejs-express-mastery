c86f673d025931863a31456f1fdcaf271bcaee0e
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-10 16:57:36 -0700

    feat: add get page 500 to controller

--- 12_throwing_error/controllers/error.js
+++ 12_throwing_error/controllers/error.js
@@ -1,3 +1,6 @@
 export const throw404 = (req,res,next)=>{
     res.status(404).render("404",{pageTitle:"Page Not Found",path:""});
 };
+export const throw500 = (req,res,next)=>{
+    res.status(500).render("500",{pageTitle:"Error!",path:"/500"});
+};
