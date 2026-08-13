55fd7a9faba72cd2634c82876ea62b81b876db69
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-13 13:48:06 -0700

    feat: add get invoice

--- 13_fileUpload/Routes/shop.js
+++ 13_fileUpload/Routes/shop.js
@@ -1,6 +1,6 @@
 import express from "express";
 // add getShop controller
-import { getProducts,getProduct , getIndex , getCart, postCart, getCheckout,getOrders, postCartDeleteProduct, postOrder, getInvoice} from "../controllers/shop.js";
+import { getProducts,getProduct , getIndex , getCart, postCart, getCheckout,getOrders, postCartDeleteProduct, postOrder, getInvoice, generateInvoice, generateInvoiceV2} from "../controllers/shop.js";
 import { isAuth } from "../middleware/is-auth.js";
 const router = express.Router();
 
@@ -13,7 +13,7 @@ router.post("/cart-delete-item",isAuth,postCartDeleteProduct);
 // router.get("/checkout",getCheckout)
 router.post("/create-order",isAuth,postOrder);
 router.get("/orders",isAuth,getOrders);
-router.get("/orders/:orderId",isAuth,getInvoice);
-
+router.get("/orders/:orderId",isAuth,generateInvoiceV2);
+router.get("/geninvoice",generateInvoice);
 
 export{router};
