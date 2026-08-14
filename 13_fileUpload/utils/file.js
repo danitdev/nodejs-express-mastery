90c7cdef3aa1a1b8b7a90492e1115440b3dfd63f
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-13 22:04:11 -0700

    fix: deleting the correct path

--- 13_fileUpload/utils/file.js
+++ 13_fileUpload/utils/file.js
@@ -1,10 +1,12 @@
 import fs from "fs";
-
+import path from "path";
+import rootDir from "./path.js";
 export const deleteFile = (filePath)=>{
-    fs.unlink(filePath,(err)=>{
+    const pathImages = path.join(rootDir, filePath);
+    fs.unlink(pathImages,(err)=>{
         if(err){
             throw (err);
         }
-        console.log(`Deleted file on path: ${filePath}`)
+        console.log(`Deleted file on path: ${pathImages}`)
     })
 }
