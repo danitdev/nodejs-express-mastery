4a3d6477ed8d19732fabe3108fcaa2316b6a4ee3
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-16 14:27:47 -0700

    chore: organize the project

--- 16_RESTAPIs/controllers/error.js
+++
@@ -1,6 +0,0 @@
-export const throw404 = (req,res,next)=>{
-    res.status(404).render("404",{pageTitle:"Page Not Found",path:""});
-};
-export const throw500 = (req,res,next)=>{
-    res.status(500).render("500",{pageTitle:"Error!",path:"/500"});
-};
--- 16_RESTAPIs/middleware/is-auth.js
+++
@@ -1,8 +0,0 @@
-export const isAuth = (req,res,next)=>{
-    if(!req.session.isLoggedIn) {
-        return res.redirect("/login");
-    }
-    else{
-        next();
-    }
-}
