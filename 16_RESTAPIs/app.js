9e45b5cc26f442d8e4f0ae4d02431aae58c6bfe9
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-16 14:34:14 -0700

    feat: add feed router to app.js

--- 16_RESTAPIs/app.js
+++ 16_RESTAPIs/app.js
@@ -1,9 +1,11 @@
 import express from "express"
-
+import {router as feedRouter} from "./routes/feed.js";
 
 
 const app = express();
 
+// GET /feed/posts
+app.use("/feed",feedRouter);
 
 
 app.listen(8080);
