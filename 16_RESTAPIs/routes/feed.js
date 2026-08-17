b133ea7869def6c3c77ad5b17915291a955a8973
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-16 17:09:25 -0700

    feat: connect the routes for postPost

--- 16_RESTAPIs/routes/feed.js
+++ 16_RESTAPIs/routes/feed.js
@@ -1,8 +1,8 @@
 import e from "express"
-import { getPosts } from "../controllers/feed.js";
+import { getPosts, postPost } from "../controllers/feed.js";
 const router = e.Router();
 
 router.get("/posts",getPosts);
-
+router.post("/post",postPost);
 
 export{router};
