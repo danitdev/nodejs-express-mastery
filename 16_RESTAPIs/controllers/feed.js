7d125827f0a20967f8a664b4ca2132ec265affa2
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-16 17:09:08 -0700

    feat: add a simple postPost for check the parser

--- 16_RESTAPIs/controllers/feed.js
+++ 16_RESTAPIs/controllers/feed.js
@@ -1,3 +1,18 @@
 export const getPosts =  (req,res,next)=>{
-    
+    res.status(200)
+        .json({posts:[{title:"first post",
+                       content:"this is some post :D"}]});
 };
+export const postPost = (req,res,next)=>{
+    const title = req.body.title;
+    const content = req.body.content;
+    //create a post in db
+    res.status(201).json({
+        message: "Post created successfully",
+        post:{
+            id:new Date().toISOString(),
+            title:title,
+            content:content
+        }
+     });
+}
