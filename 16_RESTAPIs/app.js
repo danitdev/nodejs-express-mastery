47c377efea4975b29b1b9e2c0fe7c6a5577fa4f5
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-16 17:08:44 -0700

    feat: use bodyParser to parse json

--- 16_RESTAPIs/app.js
+++ 16_RESTAPIs/app.js
@@ -1,8 +1,12 @@
-import express from "express"
+import express from "express";
+import bodyParser from "body-parser";
 import {router as feedRouter} from "./routes/feed.js";
 
 
 const app = express();
+
+
+app.use(bodyParser.json()); //application/json
 
 // GET /feed/posts
 app.use("/feed",feedRouter);
